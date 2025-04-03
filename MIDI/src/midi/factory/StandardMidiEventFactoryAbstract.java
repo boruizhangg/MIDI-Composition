2e70f02c784a355942240477e3f8274a5c832756
eteva <etevans2327@gmail.com>
2025-04-03 18:21:14 -0500

    Added unimplemented method from MidiFactoryAbstract to standard abstract

--- MIDI/src/midi/factory/StandardMidiEventFactoryAbstract.java
+++ MIDI/src/midi/factory/StandardMidiEventFactoryAbstract.java
@@ -1,5 +1,10 @@
 package midi.factory;
 
-public class StandardMidiEventFactoryAbstract {
+public class StandardMidiEventFactoryAbstract implements MidiEventFactoryAbstract{
+	
+	@Override
+	public MidiEventFactory createFactory() {
+		return new StandardMidiEventFactory();
+	}
 
 }
