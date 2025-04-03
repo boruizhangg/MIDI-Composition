e659c0a8af4399368bef47b42cc4297bfe1ddea1
eteva <etevans2327@gmail.com>
2025-04-03 18:07:57 -0500

    Added JavaDoc to MidiEventFactoryAbstract interface

--- MIDI/src/midi/factory/MidiEventFactoryAbstract.java
+++ MIDI/src/midi/factory/MidiEventFactoryAbstract.java
@@ -1,7 +1,15 @@
 package midi.factory;
 
+/**
+ * Abstract factory interface for creating MIDI event factories
+ */
 public interface MidiEventFactoryAbstract {
 	
+	/**
+	 * Creates a specific MIDI event factory
+	 * 
+	 * @return A concrete MidiEventFactory implementation
+	 */
 	MidiEventFactory createFactory();
 
 }
