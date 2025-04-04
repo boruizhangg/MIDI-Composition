5dfec59b570879fb5daf98a99d7ba38f69247c13
Borui Zhang <borui@botenders.com>
2025-04-03 20:36:40 -0500

    OOP!
    apparently that was not the right commit, here it is.

--- MIDI/src/midi/util/MidiCsvParser.java
+++ MIDI/src/midi/util/MidiCsvParser.java
@@ -1,13 +1,35 @@
 package midi.util;
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.sound.midi.ShortMessage;
+import midi.model.MidiEventData;
 
 
 
-public class MidiCswParser {
-
+public class MidiCsvParser {
+	
+	public static List<MidiEventData> parseCsv(String path) 
+	{
+		List<MidiEventData> midiEvents = new ArrayList<MidiEventData>();
+		BufferedReader reader = new BufferedReader(new FileReader(path));
+		String line = reader.readLine();
+		
+		while (line != null)
+		{
+				String[] data = line.split(",");
+				int startEndTick = grabData(data[0]);
+				String noteCommand = data[1].trim();
+				int channel = grabData(data[2]);
+				int note = grabData(data[3]);
+				int velocity = grabData(data[4]);
+				int instrument = grabData(data[5]);
+		}
+			
+		}
+	}
+	reader.close();
+	return //needs to create event in the parser
 }
