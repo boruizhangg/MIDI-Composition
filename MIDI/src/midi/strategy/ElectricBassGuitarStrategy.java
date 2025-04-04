049d8bb4e26325fda36fb2b20f3d6fdd0d16825e
Hao Ouyang <121321758+joe520088@users.noreply.github.com>
2025-04-03 22:16:38 -0500

    Update ElectricBassGuitarStrategy.java

--- MIDI/src/midi/strategy/ElectricBassGuitarStrategy.java
+++ MIDI/src/midi/strategy/ElectricBassGuitarStrategy.java
@@ -5,7 +5,7 @@ import javax.sound.midi.MidiEvent;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
-public class ElectricBassGuitarStrategy {
+public class ElectricBassGuitarStrategy implements InstrumentStrategy{
   private static final int ELECTRIC_BASS_GUITAR = 33;
 	
 	@Override
