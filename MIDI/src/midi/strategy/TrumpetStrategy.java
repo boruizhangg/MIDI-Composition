277ffd91fe4f89d856601c20d72699dd7dbb8ec7
Hao Ouyang <121321758+joe520088@users.noreply.github.com>
2025-04-03 22:19:01 -0500

    Update TrumpetStrategy.java
    implement

--- MIDI/src/midi/strategy/TrumpetStrategy.java
+++ MIDI/src/midi/strategy/TrumpetStrategy.java
@@ -5,7 +5,7 @@ import javax.sound.midi.MidiEvent;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
-public class TrumpetStrategy {
+public class TrumpetStrategy implements InstrumentStrategy{
   	private static final int TRUMPET = 56;
 	@Override
 	public void applyInstrument(Track track, int channel) 
