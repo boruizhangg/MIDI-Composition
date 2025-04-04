51b19a3f62f98fafb8c4d8982e356cc0f1218f14
Hao Ouyang <121321758+joe520088@users.noreply.github.com>
2025-04-03 22:15:33 -0500

    Update InstrumentStrategy.java
    interface class

--- MIDI/src/midi/strategy/InstrumentStrategy.java
+++ MIDI/src/midi/strategy/InstrumentStrategy.java
@@ -2,6 +2,6 @@ package midi.strategy;
 
 import javax.sound.midi.Track;
 
-public class InstrumentStrategy {
-  void applyInstrument(Track track, int channel);
+public interface InstrumentStrategy {
+	void applyInstrument(Track track, int channel);
 }
