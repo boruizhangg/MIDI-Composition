0f341bb674c4badf522a148ef6ce7b85f0a4c137
Hao Ouyang <121321758+joe520088@users.noreply.github.com>
2025-04-03 22:20:21 -0500

    Update LowerPitchStrategy.java
    implement

--- MIDI/src/midi/strategy/LowerPitchStrategy.java
+++ MIDI/src/midi/strategy/LowerPitchStrategy.java
@@ -1,6 +1,6 @@
 package midi.strategy;
 
-public class LowerPitchStrategy {
+public class LowerPitchStrategy implements PitchStrategy{
 //two more less note for lower pitch
 @Override
 	public int modifyPitch(int note) 
