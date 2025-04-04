a6f2df9e24c060cb56f0657d1176b55dee148042
Hao Ouyang <121321758+joe520088@users.noreply.github.com>
2025-04-03 21:55:16 -0500

    Update ElectricBassGuitarStrategy.java
    adding try and catch for instrument

--- MIDI/src/midi/strategy/ElectricBassGuitarStrategy.java
+++ MIDI/src/midi/strategy/ElectricBassGuitarStrategy.java
@@ -1,5 +1,21 @@
 package midi.strategy;
 
 public class ElectricBassGuitarStrategy {
-
+  private static final int ELECTRIC_BASS_GUITAR = 33;
+	
+	@Override
+	public void applyInstrument(Track track, int channel) 
+	{
+		try
+		{
+			ShortMessage instrumentChange = new ShortMessage();
+			instrumentChange.setMessage(ShortMessage.PROGRAM_CHANGE, channel, ELECTRIC_BASS_GUITAR, 0);
+			track.add(new MidiEvent(instrumentChange, 0));
+		}
+		catch(InvalidMidiDataException e)
+		{
+			System.err.println("Erro setting Electric Bass Guitar instrument: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 }
