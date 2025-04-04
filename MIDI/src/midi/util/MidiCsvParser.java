7f42f47f1e284c5a6f028efd4fd5da99ee448f03
Borui Zhang <borui@botenders.com>
2025-04-03 21:34:58 -0500

    Finished grabData helper function read msg body for more details
    I am assuming the format of csv files fed to this will be the same as
    the one provided initially, so I am not putting a parameter check for
    the bounds of a line of entry. In addition, if anyone want to optomize
    this please feel free because using scanner is not very efficient.

--- MIDI/src/midi/util/MidiCsvParser.java
+++ MIDI/src/midi/util/MidiCsvParser.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.sound.midi.ShortMessage;
 import midi.model.MidiEventData;
+import java.io.IOException;
 
 
 
 public class MidiCsvParser {
 	
-	public static List<MidiEventData> parseCsv(String path) 
+	public static List<MidiEventData> parseCsv(String path) throws IOException
 	{
 		List<MidiEventData> midiEvents = new ArrayList<MidiEventData>();
 		BufferedReader reader = new BufferedReader(new FileReader(path));
@@ -20,16 +21,43 @@ public class MidiCsvParser {
 		while (line != null)
 		{
 				String[] data = line.split(",");
+				
+				int noteOnOff;
 				int startEndTick = grabData(data[0]);
-				String noteCommand = data[1].trim();
+				String onOff = data[1].trim();
 				int channel = grabData(data[2]);
 				int note = grabData(data[3]);
 				int velocity = grabData(data[4]);
 				int instrument = grabData(data[5]);
-		}
+				
+				if(onOff.equals("Note_on_c"))
+				{
+					noteOnOff = ShortMessage.NOTE_ON;
+				}
+				else
+				{
+					noteOnOff = ShortMessage.NOTE_OFF;
+				}
+				
+				MidiEventData event = new MidiEventData(startEndTick, velocity, note, channel, instrument, noteOnOff);
+				midiEvents.add(event);
 			
+			line = reader.readLine();
 		}
-	}
-	reader.close();
-	return //needs to create event in the parser
+	
+		reader.close();
+		return midiEvents;
+}
+
+	private static int grabData(String str)
+	{
+		Scanner scanner = new Scanner(str.trim());
+		int data = 0;
+		if(scanner.hasNextInt())
+		{
+			data = scanner.nextInt();
+		}
+		scanner.close();
+		return data;
+	}	
 }
