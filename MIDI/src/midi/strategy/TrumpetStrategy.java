5b264ee7a2a7446fa4047c8a0d08bedb866c976b
Hao Ouyang <121321758+joe520088@users.noreply.github.com>
2025-04-03 21:59:42 -0500

    Update TrumpetStrategy.java
    using try and catch

--- MIDI/src/midi/strategy/TrumpetStrategy.java
+++ MIDI/src/midi/strategy/TrumpetStrategy.java
@@ -1,5 +1,27 @@
 package midi.strategy;
 
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+
 public class TrumpetStrategy {
+  	private static final int TRUMPET = 56;
+	@Override
+	public void applyInstrument(Track track, int channel) 
+	{
+		try
+		{
+			ShortMessage instrumentChange = new ShortMessage();
+			instrumentChange.setMessage(ShortMessage.PROGRAM_CHANGE, channel, TRUMPET, 0);
+			track.add(new MidiEvent(instrumentChange, 0));
+		}
+		catch(InvalidMidiDataException e)
+		{
+			System.err.println("Erro setting Electric Bass Guitar instrument: " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+	}
 
 }
